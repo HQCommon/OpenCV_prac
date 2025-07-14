06814a4c010abf4e09d71abc09a0d0be8c420956
Thyler Cruz <Thylercruz@outlook.com>
2025-07-14 14:06:37 -0500

    basic.py update

--- basic.py
+++ basic.py
@@ -8,4 +8,11 @@ cv.imshow('Cat', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+# Blur
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+# Edge Cascade
+
+
 cv.waitKey(0)
